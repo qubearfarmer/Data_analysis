ffebd0484a01e5d9f580d1d0ec52ba960d8fc0eb
nguyen89 <nbaolong89@gmail.com>
2016-12-17 17:59:43 -0500

    update plot with coupling to cavity

--- T1_automation_code.py
+++ T1_automation_code.py
@@ -10,17 +10,19 @@ warnings.simplefilter("error", RuntimeWarning)
 def func(x, a, b, c, d):
     return a*np.exp(-(x-c)/b) + d
 
+#Path for saving file
 directory = 'G:\Projects\Fluxonium\Data\Summary of T1_T2_vs flux_Fluxonium#10\Automation code'
 fname = 'T1_rabi_35to36p1mA.csv'
 path_s = directory + '\\' + fname
 
-#File path
-directory = 'D:\Data\Fluxonium #10_New software'
-measurement = 'T1_auto_38.5to38.6mA_1.h5'
+#Path to read data
+directory = 'G:\Projects\Fluxonium\Data\Fluxonium #10_New software'
+measurement = 'T1_auto_35to36p1mA_1.h5'
 path = directory + '\\' + measurement
 # time_fname = 'T1_delays_35to36p1mA.txt'
 # path_t = directory + '\\' + time_fname
-t_inc = 10
+t_inc = 3e3 #unit in ns
+t1_guess = 20e-6 #Unit in s
 
 #Read data and fit
 time = np.linspace(0, 20*t_inc, 20)
@@ -32,64 +34,65 @@ with h5py.File(path,'r') as hf:
     freq_raw = np.array(hf.get('freq'))
     flux_raw = np.array(hf.get('flux'))
     total_count = len(flux_raw)
-    T1 = []
-    T1_error = []
+    T1_array = []
+    T1_err_array = []
     freq = []
     flux = []
     RabiA = []
-    print 'Total points taken ' + str(total_count)
     for idx in range(total_count):
         phase = -phase_raw[idx, 0]
         phase = np.unwrap(phase)
         phase = phase - np.min(phase)
         phase = phase*180/np.pi
-        guessA= phase[0]-phase[-1]
+        guessA= np.max(phase) - np.min(phase)
         # plt.plot(phase)
         # print freq_raw[idx]
         if guessA < 0.5 or guessA > 10:
             continue
-        guess = [guessA, 20e-6, 0, 0]
+        guess = [guessA, t1_guess, 0, 0]
         try:
-            popt, pcov = curve_fit(func, time*1e-6, phase, guess)
+            popt, pcov = curve_fit(func, time*1e-9, phase, guess)
         except RuntimeError:
-            print "Cannot fit entry " + str(idx)
+            # print "Cannot fit entry " + str(idx)
             continue
         except OptimizeWarning:
-            print "Doesn't fit well entry " + str(idx)
+            # print "Doesn't fit well entry " + str(idx)
             continue
         except RuntimeWarning:
-            print "Doesn't fit well entry " + str(idx)
+            # print "Doesn't fit well entry " + str(idx)
             continue
         a,b,c,d = popt #b is T1
-        phase_fit = func(time*1e-6, a, b, c, d)
+        phase_fit = func(time*1e-9, a, b, c, d)
         perr = np.sqrt(abs(np.diag(pcov)))
-        if b*1e6 < t_inc or b*1e6 > t_inc*19 or a < 0.5 or a > 10:# or perr[1]*1e6 > t_inc:
+        T1 = b*1e6 #unit is us
+        T1_err = perr[1]*1e6
+        if b*1e6 < t_inc/1e3 or b*1e6 > t_inc/1e3*19 or a < 0.5 or a > 10 or T1_err > T1*0.2:
             continue
-        T1_error = np.append(T1_error, perr[1]*1e6)
-        T1 = np.append(T1, b*1e6) #T1 in us
+        T1_err_array = np.append(T1_err_array, T1_err)
+        T1_array = np.append(T1_array, T1) #T1 in us
         flux = np.append(flux, flux_raw[idx])
         freq = np.append(freq, freq_raw[idx])
         RabiA = np.append(RabiA, a)
-        print b*1e6, perr[1]*1e6
-        if b*1e6 > 20:
-            plt.plot(time, phase, time, phase_fit)
-            plt.title(str(b*1e6))
-            plt.show()
+        print T1, T1_err
+        plt.plot(time,phase)
+        # if b*1e6 > 20:
+        #     plt.plot(time/1e3, phase, time/1e3, phase_fit)
+        #     plt.title(str(b*1e6))
+        #     plt.show()
 
-# plt.plot(T1, 'r.')
-# directory = 'G:\Projects\Fluxonium\Data\Summary of T1_T2_vs flux_Fluxonium#10\Automation code'
-# fname = 'T1_rabi_38p5to38p6mA.csv'
-# # fname ='dummy.csv'
-# path = directory + '\\' + fname
-# file = np.zeros((len(flux),4))
-# file[:,0] = flux
-# file[:,1] = freq
-# file[:,2] = T1
-# file[:,3] = RabiA
-# print 'pts fitted well ' + str(len(T1))
-# #Check
-# np.savetxt(path_s, file)
+###############################################################################
+#Save file here
+###############################################################################
+data = np.zeros((len(flux),5))
+data[:,0] = flux
+data[:,1] = freq
+data[:,2] = T1_array
+data[:,3] = T1_err_array
+data[:,4] = RabiA
+print 'Total points taken ' + str(total_count)
+print 'pts fitted well ' + str(len(T1_array))
+#Check
+np.savetxt(path_s, data, delimiter =',', header='YOKO, Freq, T1, T1_err, Amp')
 # data = np.genfromtxt(path_s)
-# plt.show()
 # plt.plot(data[:,0], data[:,2], 'ro')
-# plt.show()
+plt.show()
--- T1_loop_fit.py
+++ T1_loop_fit.py
@@ -1,4 +1,3 @@
-
 import numpy as np
 from matplotlib import pyplot as plt
 from scipy.optimize import curve_fit
@@ -8,30 +7,37 @@ warnings.simplefilter('error', OptimizeWarning)
 plt.figure(figsize=(5, 5))
 plt.rc('font', family='serif')
 
+#Fitting function
 def T1_func(x,a,b,c,d):
     return a*np.exp(-(x-c)/b) + d
 
-directory = "G:\Projects\Fluxonium\Data\Fluxonium #10_python code by Jon\T1_T2_(0to1)_YOKO_38p5to38p76mA\T1_T2_(0to1)_YOKO_38p76mA"
-measurement = 't1_pulse_2.951e9_708'
+#Directory for reading data
+directory = "G:\Projects\Fluxonium\Data\Fluxonium #10_python code by Jon\T1_T2_(0to1)_YOKO_38p5to38p76mA\T1_T2_(0to1)_YOKO_38p68mA"
+measurement = 't1_pulse_3.800e9_653'
 
-current_uncorr = 38.76
-current = 38.74
+#Information to save data
+current_uncorr = 38.68
+current = 38.663
+freq = 3.8
 fname = 'T1_T1err_'+str(current_uncorr)+'mA.csv'
+#Optional: print flux here to double check
+print str(current_uncorr) +'mA'
 
+#Read data and fit here
 path = directory + '\\' + measurement
 time = np.genfromtxt(path+'_time0.csv')
 phase_avg = np.zeros(len(time))
-T1_guess = 1500e-6
+T1_guess = 1500e-6 #Provide guess here
 count = 0
 T1_array = []
 T1_err_array = []
-for idx in range(0,10):
+for idx in range(0,10): #can change the range and indices here
     phase = np.genfromtxt(path + '_phase' + str(idx)+'.csv')
     phase = -phase*180/np.pi
     phase = phase - np.min(phase)
     guessA = np.max(phase)-np.min(phase)
     guess =[guessA, T1_guess, 0, 0]
-    if guessA < 0.3 :
+    if guessA < 0.3 : #Change as necessary. Observation of the raw data would suffice in determining the threshold.
         continue
     try:
         popt,pcov = curve_fit(T1_func, time*1e-9, phase, guess)
@@ -47,15 +53,17 @@ for idx in range(0,10):
     T1_err_array = np.append(T1_err_array, T1_err)
     phase_avg = phase_avg + phase
     count = count + 1
+    #Plot raw data
     print T1, ',', T1_err
     plt.plot(time,phase)
 
 ###################################################################
 #Save file
 directory = 'G:\Projects\Fluxonium\Data\Summary of T1_T2_vs flux_Fluxonium#10\Corrected flux\Individual fits'
-data = np.zeros((count, 4))
+data = np.zeros((count, 5))
 data[0:,0] = np.ones(count)*current
-data[0:,1] = np.ones(count)*current_uncorr
+data[0:,1] = np.ones(count)*freq
+data[0:,4] = np.ones(count)*current_uncorr
 data[0:,2] = T1_array
 data[0:,3] = T1_err_array
 path = directory + '\\' + fname
@@ -63,6 +71,7 @@ np.savetxt(path, data, delimiter =',', header='YOKO, YOKO_uncorr, T1, T1_err')
 #####################################################################
 ############################AVG PHASE################################
 #####################################################################
+#For comparision and conclusion purposes, calculate the average phase and fit it here
 phase_avg = phase_avg / count
 guessA = np.max(phase_avg)-np.min(phase_avg)
 guess =[guessA, T1_guess, 0, 0]
