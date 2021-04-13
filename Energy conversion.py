f297bd01d25704419a49460bf47d97c111cf01f7
qubearfarmer <nbaolong89@gmail.com>
2021-04-13 00:00:28 -0700

    update notebooks

--- Energy conversion.py
+++ Energy conversion.py
@@ -4,8 +4,8 @@ e = 1.602e-19
 h = 6.626e-34
 
 #HFSS calculation
-freq = 6.1935e9
-L = 30e-9
+freq = 5.369e9
+L = 60e-9
 C_shunt = ((freq*2*np.pi)**2*L)**-1.0*1e15
 print ('Shunting capacitance: '+str(round(C_shunt,4))+' fF')
 junc_area = 0.1*0.1     #um x um
