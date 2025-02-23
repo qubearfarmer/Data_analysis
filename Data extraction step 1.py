de7318678d7969480cc507560795eb65036518a3
qubearfarmer <nbaolong89@gmail.com>
2025-02-23 10:20:31 -0800

    update

--- Data extraction step 1.py
+++ Data extraction step 1.py
@@ -5,36 +5,54 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.cm as cm
 from matplotlib import rc
+import pandas as pd
 rc('text', usetex=False)
 
 #Enter directory and name of measurement
-directory = 'G:\Projects\Fluxonium & qubits\Data\\2016_01\\13'
-measurement = 'S21_Phase2tones_ZNB_0&n40_BW100Hz_SMB_n20dBm_6to8GHz_YOKO_n7ton10V'
-path_data = directory + '\\' + measurement + '_Phase.csv'
-path_freq = directory + '\\' + measurement + '_Freq.csv'
-path_vol = directory + '\\' + measurement + '_Voltage.csv'
+using_mac = True
+if using_mac == True:
+    data_directory = r'/Users/longbnguyen/Library/Mobile Documents/com~apple~CloudDocs/gridium_data//'
+    fig_directory = r'/Users/longbnguyen/Library/Mobile Documents/com~apple~CloudDocs/gridium_paper/Illustration//'
+    cd_sym = "//"
+fig, ax = plt.subplots(figsize = [5,4])
 
-RawData = np.genfromtxt(path_data, delimiter =',')
-Freq = np.genfromtxt(path_freq, delimiter =',')/1e9
-V = np.genfromtxt(path_vol, delimiter =',')
+################
+# fname = data_directory + 'Formatted_data' + cd_sym + 'Soft_Gridium_Two_Tone_phi_sweep_theta=0.csv'
+# df = pd.read_csv(fname)
 
-Z = RawData.transpose()
-          
-#Optional: calculate differential
-Z_diff = np.diff(Z.transpose()) 
-Z_diff = Z_diff.transpose()
+# freq = np.linspace(0.01, 8.01, 1601)
+# flux = np.linspace(0,1,301)
+# data=df['amplitude']
+# data = np.array(data)
+# data=data.reshape((301,1601))
 
-#Plot the intensity map
-X, Y = np.meshgrid(V,Freq)
-fig=plt.figure()
-plt.pcolormesh(X, Y, Z, cmap=cm.BuGn, vmin =-1 , vmax =1)
-plt.title(measurement)
-plt.xlabel("Voltage (V)")
-plt.ylabel("Frequency (GHz)")
-plt.colorbar()
+# X,Y = np.meshgrid(flux+0.004,freq)
+# for idx in range(301):
+#     data[idx,:] = data[idx,:] - np.average(data[idx,:])
+#     # if np.max(data[idx,:]) > 4 or np.min(data[idx,:]) < -4:
+#     #     data[idx,:] = [0]*1601
+# Z = (data).transpose()
+# axes=ax.pcolormesh(X,Y,Z, cmap = 'RdBu_r', vmin = -10.01, vmax = 10.01)
+############################
 
+fname = data_directory + 'Formatted_data' + cd_sym+'EJ=7_Gridium_Two_Tone_phi_sweep_theta=pi.csv'
+df = pd.read_csv(fname)
+freq = np.unique(df.frequency)*1e-9
+current = np.unique(df.Current)*1e6
+amplitude = np.reshape(df.amplitude, [len(current), len(freq)])
+phase = np.reshape(df.phase, [len(current), len(freq)])
+phi_ext = np.linspace(0,1,len(current))
+
+X,Y = np.meshgrid(phi_ext, freq)
+Z = amplitude.transpose()
+Z = phase.transpose()
+for idx in range(len(current)):
+    Z[:,idx] = Z[:,idx]-np.average(Z[:,idx])
+axes=ax.pcolormesh(X,Y,Z, cmap = 'RdBu_r', vmin = -2, vmax = 2)
+###############################################  
+#   
 #Click on the points on screen to define an approximation line for interpolation
 def onclick(event):
-    print '[%f, %f],'%(event.xdata, event.ydata)
+    print ('[%f, %f],'%(event.xdata, event.ydata))
 cid = fig.canvas.mpl_connect('button_press_event', onclick)
 plt.show()
